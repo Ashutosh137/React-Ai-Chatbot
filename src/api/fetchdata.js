48a39e262dd6f026da6cd83ce7e41c6c8fa0719f
ashutosh sharma <mr.luckysharma7@gmail.com>
2023-11-28 23:28:39 +0530

    .

--- src/api/fetchdata.js
+++ src/api/fetchdata.js
@@ -1,19 +1,21 @@
 const { Configuration, OpenAIApi } = require("openai");
-const OPENAI_API_KEY = APIKEY;
+
+const OPENAI_API_KEY = process.env.REACT_APP_OPENAI_APIKEY;
+console.log(process.env.REACT_APP_OPENAI_APIKEY)
 const configuration = new Configuration({
   apiKey: OPENAI_API_KEY,
 });
 
-const Friends= async (ask) => {
+const Friends = async (ask) => {
   const openai = new OpenAIApi(configuration);
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     messages: ask,
-    temperature:0.1,
+    temperature: 0.1,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0.6,
-    
+
   }).catch((err) => {
     console.log(err);
   });
@@ -38,5 +40,5 @@ const Teachers = async (ask) => {
   return data;
 };
 
-export {Teachers,Friends};
+export { Teachers, Friends };
 
